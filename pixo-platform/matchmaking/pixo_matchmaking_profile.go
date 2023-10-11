c33d5a04d737c623ec3c7b039343f82b08105579
josephreederpixovr <joseph.reeder@pixovr.com>
2023-10-11 10:22:30 -0400

    PLATFORM-781 (dev): moving matchmaking profile models to shared

---
+++ pixo-platform/matchmaking/pixo_matchmaking_profile.go
@@ -0,0 +1,31 @@
+package matchmaking
+
+import (
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/wrappers"
+	"github.com/rs/zerolog/log"
+	"open-match.dev/open-match/pkg/pb"
+)
+
+const (
+	MaxPlayersExtensionKey = "max-players"
+)
+
+type PixoMatchmakingProfile struct {
+	*pb.MatchProfile
+}
+
+func NewPixoMatchmakingProfile(p *pb.MatchProfile) *PixoMatchmakingProfile {
+	return &PixoMatchmakingProfile{MatchProfile: p}
+}
+
+func (p *PixoMatchmakingProfile) GetMaxPlayersPerMatch() int {
+	var val wrappers.Int32Value
+	if err := ptypes.UnmarshalAny(p.MatchProfile.GetExtensions()[MaxPlayersExtensionKey], &val); err != nil {
+		log.Error().Err(err).Msg("Cannot retrieve number of players per match")
+		return 1
+	} else {
+		log.Debug().Msgf("Max players per match: %d", val.Value)
+		return int(val.Value)
+	}
+}
