b7160b75ecbd6179695c9b6e10cd18077b51e8c3
Walker O'Brien <walker.obrien@pixovr.com>
2023-11-08 21:51:47 -0600

    return stream

--- pixo-platform/k8s/base/logs.go
+++ pixo-platform/k8s/base/logs.go
@@ -1,18 +1,16 @@
 package base
 
 import (
-	"bytes"
 	"context"
-	"github.com/rs/zerolog/log"
 	"io"
 	corev1 "k8s.io/api/core/v1"
 )
 
-func (c Client) GetPodLogs(ctx context.Context, namespace, podName, containerName string) (string, error) {
+func (c Client) GetPodLogs(ctx context.Context, namespace, podName, containerName string) (io.ReadCloser, error) {
 
 	podLogOpts := corev1.PodLogOptions{
 		Container: containerName,
-		TailLines: &[]int64{100}[0],
+		//TailLines: &[]int64{100}[0],
 	}
 
 	req := c.
@@ -20,25 +18,5 @@ func (c Client) GetPodLogs(ctx context.Context, namespace, podName, containerNam
 		Pods(namespace).
 		GetLogs(podName, &podLogOpts)
 
-	podLogs, err := req.Stream(ctx)
-	if err != nil {
-		log.Err(err).Msgf("Error in opening stream: %s", err)
-		return "", err
-	}
-
-	defer func(podLogs io.ReadCloser) {
-		err = podLogs.Close()
-		if err != nil {
-			log.Err(err).Msgf("Error in closing stream: %s", err)
-		}
-	}(podLogs)
-
-	buf := new(bytes.Buffer)
-	_, err = io.Copy(buf, podLogs)
-	if err != nil {
-		return "", err
-	}
-	str := buf.String()
-
-	return str, err
+	return req.Stream(ctx)
 }
