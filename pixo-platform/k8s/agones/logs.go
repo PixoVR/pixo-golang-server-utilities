35ad80ddd825849c563fcc6edeb8d33d09435cf4
Walker O'Brien <walker.obrien@pixovr.com>
2023-11-08 22:20:57 -0600

    move logs to base

--- pixo-platform/k8s/agones/logs.go
+++ pixo-platform/k8s/agones/logs.go
@@ -2,12 +2,10 @@ package agones
 
 import (
 	agonesv1 "agones.dev/agones/pkg/apis/agones/v1"
-	"bytes"
 	"context"
 	"errors"
 	"github.com/PixoVR/pixo-golang-server-utilities/pixo-platform/k8s/base"
 	log "github.com/rs/zerolog/log"
-	"io"
 )
 
 func (c Client) GetGameServerLogs(ctx context.Context, baseClient *base.Client, namespace string, gs agonesv1.GameServer) (string, string, error) {
@@ -29,21 +27,5 @@ func (c Client) GetGameServerLogs(ctx context.Context, baseClient *base.Client, 
 		return "", "", err
 	}
 
-	return ReadLogsFromStream(logs), ReadLogsFromStream(sidecarLogs), nil
-}
-
-func ReadLogsFromStream(stream io.ReadCloser) string {
-
-	defer func(podLogs io.ReadCloser) {
-		if err := podLogs.Close(); err != nil {
-			log.Err(err).Msg("Error in closing stream")
-		}
-	}(stream)
-
-	buf := new(bytes.Buffer)
-	if _, err := io.Copy(buf, stream); err != nil {
-		return "unable to read logs"
-	}
-
-	return buf.String()
+	return base.ReadLogsFromStream(logs), base.ReadLogsFromStream(sidecarLogs), nil
 }
--- pixo-platform/k8s/base/logs.go
+++ pixo-platform/k8s/base/logs.go
@@ -1,7 +1,9 @@
 package base
 
 import (
+	"bytes"
 	"context"
+	"github.com/rs/zerolog/log"
 	"io"
 	corev1 "k8s.io/api/core/v1"
 )
@@ -19,4 +21,20 @@ func (c Client) GetPodLogs(ctx context.Context, namespace, podName, containerNam
 		GetLogs(podName, &podLogOpts)
 
 	return req.Stream(ctx)
+}
+
+func ReadLogsFromStream(stream io.ReadCloser) string {
+
+	defer func(podLogs io.ReadCloser) {
+		if err := podLogs.Close(); err != nil {
+			log.Err(err).Msg("Error in closing stream")
+		}
+	}(stream)
+
+	buf := new(bytes.Buffer)
+	if _, err := io.Copy(buf, stream); err != nil {
+		return "unable to read logs"
+	}
+
+	return buf.String()
 }
