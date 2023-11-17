4d444522b61a9afd3095b8e1a07d63aee6c67c93
Walker O'Brien <walker.obrien@pixovr.com>
2023-11-17 00:32:03 -0600

    add gin ctx getter

--- pixo-platform/config/context.go
+++ pixo-platform/config/context.go
@@ -1,7 +1,25 @@
 package config
 
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	GinContextKey = "GIN_CONTEXT"
+)
+
 type ContextRequest string
 
 func (c ContextRequest) String() string {
 	return string(c)
+}
+
+func GetGinContext(ctx context.Context) *gin.Context {
+	ginContext, ok := ctx.Value(GinContextKey).(*gin.Context)
+	if !ok {
+		return nil
+	}
+
+	return ginContext
 }
