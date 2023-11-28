d68e6e6b886db4b2d3abe9d9f4584f017a4a6c39
Walker O'Brien <walker.obrien@pixovr.com>
2023-11-28 00:33:12 -0600

    warning

--- pixo-platform/config/env.go
+++ pixo-platform/config/env.go
@@ -16,7 +16,7 @@ func LoadEnvVars(differential ...string) {
 	envPath := filepath.Join(GetProjectRoot(differential...), ".env")
 
 	if err := godotenv.Load(envPath); err != nil {
-		log.Warn().Msgf("No .env file loaded: %s", err)
+		log.Debug().Msgf("No .env file loaded: %s", err)
 	}
 }
 
