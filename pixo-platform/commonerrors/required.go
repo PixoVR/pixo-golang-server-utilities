10cfbd7e681f002b3650f77cdbdff392ef58c68e
Walker O'Brien <walker.obrien@pixovr.com>
2024-01-10 11:52:06 -0600

    dec redis and organize common errors

--- pixo-platform/commonerrors/required.go
+++ pixo-platform/commonerrors/required.go
@@ -7,9 +7,3 @@ import (
 func ErrorRequired(fieldName string) error {
 	return errors.New(fieldName + " is required")
 }
-
-const DefaultRecordNotFoundMessage = "record not found"
-
-func ErrorNotFound(objectType string) error {
-	return errors.New(objectType + " not found")
-}
