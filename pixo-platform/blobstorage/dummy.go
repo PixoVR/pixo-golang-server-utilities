ca148ecb3e6e8263c8f21ada0fdc0576b445173f
Walker O'Brien <walker.obrien@pixovr.com>
2024-02-07 20:24:37 -0600

    add dummy

---
+++ pixo-platform/blobstorage/dummy.go
@@ -0,0 +1,49 @@
+package client
+
+import (
+	"context"
+	"io"
+)
+
+type StorageClientSpy struct {
+	GetSignedURLNumTimesCalled        int
+	UploadFileNumTimesCalled          int
+	FileExistsNumTimesCalled          int
+	ReadFileNumTimesCalled            int
+	DeleteFileNumTimesCalled          int
+	InitResumableUploadNumTimesCalled int
+}
+
+func NewStorageClientSpy() StorageClient {
+	return &StorageClientSpy{}
+}
+
+func (f *StorageClientSpy) GetSignedURL(ctx context.Context, object UploadableObject) (string, error) {
+	f.GetSignedURLNumTimesCalled++
+	return "fake-signed-url", nil
+}
+
+func (f *StorageClientSpy) UploadFile(ctx context.Context, object UploadableObject, fileReader io.Reader) (string, error) {
+	f.UploadFileNumTimesCalled++
+	return "fake-url", nil
+}
+
+func (f *StorageClientSpy) FileExists(ctx context.Context, object UploadableObject) (bool, error) {
+	f.FileExistsNumTimesCalled++
+	return true, nil
+}
+
+func (f *StorageClientSpy) ReadFile(ctx context.Context, object UploadableObject) (io.ReadCloser, error) {
+	f.ReadFileNumTimesCalled++
+	return nil, nil
+}
+
+func (f *StorageClientSpy) DeleteFile(ctx context.Context, object UploadableObject) error {
+	f.DeleteFileNumTimesCalled++
+	return nil
+}
+
+func (f *StorageClientSpy) InitResumableUpload(ctx context.Context, object UploadableObject) (*ResumableUploadResponse, error) {
+	f.InitResumableUploadNumTimesCalled++
+	return nil, nil
+}
