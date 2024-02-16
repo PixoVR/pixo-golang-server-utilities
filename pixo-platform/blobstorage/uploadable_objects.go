f340ace4dfd69bd1d147bb68b964e77243174611
Walker O'Brien <walker.obrien@pixovr.com>
2024-02-16 03:50:20 -0600

    add helper

--- pixo-platform/blobstorage/uploadable_objects.go
+++ pixo-platform/blobstorage/uploadable_objects.go
@@ -33,6 +33,10 @@ type DefaultPublicUploadable struct {
 	Path string
 }
 
+func PublicUploadable(fileLocation string) DefaultPublicUploadable {
+	return DefaultPublicUploadable{Path: fileLocation}
+}
+
 func (p DefaultPublicUploadable) GetBucketName() string {
 	return os.Getenv("GOOGLE_STORAGE_PUBLIC")
 }
