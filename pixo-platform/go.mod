bedc7b189e8b89d48e3ca7479edc73f96a365c77
Walker O'Brien <walker.obrien@pixovr.com>
2024-05-20 20:33:10 -0500

    add testing

--- pixo-platform/go.mod
+++ pixo-platform/go.mod
@@ -1,6 +1,7 @@
 module github.com/PixoVR/pixo-golang-server-utilities/pixo-platform
 
-go 1.21
+go 1.22.0
+
 toolchain go1.22.2
 
 require (
---
+++ pixo-platform/testing/api_feature.go
@@ -0,0 +1,44 @@
+package testing
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-resty/resty/v2"
+	"net/http"
+	"net/http/httptest"
+)
+
+type ServerTestFeature struct {
+	Engine   *gin.Engine
+	Recorder *httptest.ResponseRecorder
+
+	Client   *resty.Client
+	Response *http.Response
+
+	ID     string
+	UserID int
+	APIKey string
+	Err    error
+	Token  string
+
+	DirectoryFilePath string
+	GraphQLOperation  string
+	SendFileKey       string
+	SendFile          string
+	GraphQLResponse   map[string]interface{}
+}
+
+func (s *ServerTestFeature) resetResponse(interface{}) {
+	s.Engine = gin.Default()
+	s.Recorder = httptest.NewRecorder()
+
+	s.Client = resty.New()
+	s.Response = nil
+
+	s.Token = ""
+	s.APIKey = ""
+	s.Err = nil
+	s.GraphQLResponse = make(map[string]interface{})
+	s.DirectoryFilePath = ""
+	s.SendFileKey = ""
+	s.SendFileKey = ""
+}
